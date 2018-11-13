21c330b0a94a0b8d4fa0c1c823e6b2761799487a
Bingo Bobo <matmazur90@gmail.com>
2018-11-13 20:59:05 +0100

    added POJO Product

---
+++ src/main/java/com/matmazur/springjpa/model/Product.java
@@ -0,0 +1,66 @@
+package com.matmazur.springjpa.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double price;
+    private String details;
+
+    public Product() {
+    }
+
+    public Product(String name, Double price, String details) {
+        this.name = name;
+        this.price = price;
+        this.details = details;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", details='" + details + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+}
