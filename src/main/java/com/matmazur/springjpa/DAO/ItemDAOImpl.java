330a8707f4ded2fc60a2da6ae736134be052802a
Bingo Bobo <matmazur90@gmail.com>
2018-12-06 15:00:01 +0100

    ItemDAOImpl extends GenericDAO abstract class, 2 additional typedqueries added to ItemDAO for delete all items and getting whole list of items

---
+++ src/main/java/com/matmazur/springjpa/DAO/ItemDAOImpl.java
@@ -0,0 +1,27 @@
+package com.matmazur.springjpa.DAO;
+
+import com.matmazur.springjpa.model.Item;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import java.lang.reflect.Type;
+import java.util.List;
+
+@Repository
+@Transactional
+public class ItemDAOImpl extends GenericDAO<Item, Long> {
+
+public List<Item> getAll(){
+    TypedQuery<Item> getAllQuery = entityManager.createQuery("SELECT i FROM Item i ",Item.class);
+    return getAllQuery.getResultList();
+}
+
+public void deleteAll(){
+    TypedQuery<Item> query = entityManager.createQuery("DELETE FROM Item i",Item.class);
+    query.executeUpdate();
+
+}
+
+
+}
